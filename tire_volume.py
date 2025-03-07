18b864eea12a4053a8eca9f402c53734b0bfe4ad
Lucas Felipe <lucas.miranda880@outlook.com>
2025-03-06 23:13:44 -0300

    tire volume programa first version

---
+++ tire_volume.py
@@ -0,0 +1,7 @@
+import math
+
+width = float(input("Enter the width of the tire in mm (ex. 205): "))
+aspect_ratio = float(input("Enter the aspect ratio of the tire (ex. 60) : "))
+diamanter_wheel = float(input("Enter the diameter of the wheel in inches (ex. 15): "))
+volume = (math.pi * width ** 2 * aspect_ratio * (width * aspect_ratio + 2540 * diamenter_wheel)) / 10000000
+print(f"The approximate volume is {volume:.1f} milliliters.")
