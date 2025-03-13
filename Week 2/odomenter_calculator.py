ba2b3d91c263d274567a102f55116bce494e2485
Lucas Felipe <lucas.miranda880@outlook.com>
2025-03-12 22:16:28 -0300

    created the first function to show the efficiency the miles per gallon

--- Week 2/odomenter_calculator.py
+++ Week 2/odomenter_calculator.py
@@ -1,12 +1,14 @@
-def main ():
-
+def main():
+    start = float(input("Enter the starting odometer reading: "))
+    end = float(input("Enter the ending odometer reading: "))
+    gallons = float(input("Enter the number of gallons of gas used: "))
+    mpg2 = miles_per_gallon(start, end, gallons)
+    print(f"The miles per gallon is {mpg2:.2f}")
 
 
 def miles_per_gallon(start_miles, end_miles, gallons):
+    mpg = (end_miles - start_miles) / gallons
     
-    return
+    return mpg
 
-def lp100k_from_mpg(mpg):
-    
-    return
 main()
