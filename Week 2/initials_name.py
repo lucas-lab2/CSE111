793d92c29553bef985bbf727f44e9f4f590900b7
Lucas Felipe <lucas.miranda880@outlook.com>
2025-03-12 21:29:32 -0300

    added the possiblity to pass multiple paramenters. Now the first and last name will be in uppercase while the middle name will show up in lowercase

--- Week 2/initials_name.py
+++ Week 2/initials_name.py
@@ -1,7 +1,11 @@
-def get_inital_name (name):
-    inital = name[0:1].upper()
-    return inital
+def get_initial_name(name, force_uppercase):
+    if force_uppercase:
+        initial = name[0:1].upper()  # get the first letter of the name and convert it to uppercase the [0:1] means that we are getting the first letter of the name
+    else:
+        initial = name[0:1]
+    return initial
 
-first_name = get_inital_name(input("Enter your first name: "))
-last_name = get_inital_name(input("Enter your last name: "))
-print("Your initials are: " + first_name + last_name)
+first_name = get_initial_name(input("Enter your first name: "), True)
+middle_name = get_initial_name(input("Enter your middle name: "), False)
+last_name = get_initial_name(input("Enter your last name: "), True)
+print("Your initials are: " + first_name + middle_name + last_name)
