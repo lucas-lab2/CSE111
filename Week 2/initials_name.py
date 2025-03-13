af1a60821fcd39836af4f1bb82bdbac7aa38c091
Lucas Felipe <lucas.miranda880@outlook.com>
2025-03-12 21:31:16 -0300

    I've added the true value to my paramenter so I don't have to pass a value to it, unless it is different

--- Week 2/initials_name.py
+++ Week 2/initials_name.py
@@ -1,11 +1,11 @@
-def get_initial_name(name, force_uppercase):
+def get_initial_name(name, force_uppercase = True):
     if force_uppercase:
         initial = name[0:1].upper()  # get the first letter of the name and convert it to uppercase the [0:1] means that we are getting the first letter of the name
     else:
         initial = name[0:1]
     return initial
 
-first_name = get_initial_name(input("Enter your first name: "), True)
+first_name = get_initial_name(input("Enter your first name: "))
 middle_name = get_initial_name(input("Enter your middle name: "), False)
-last_name = get_initial_name(input("Enter your last name: "), True)
+last_name = get_initial_name(input("Enter your last name: "))
 print("Your initials are: " + first_name + middle_name + last_name)
