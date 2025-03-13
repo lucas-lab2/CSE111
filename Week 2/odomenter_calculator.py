e420554640c225152c9c1955aaf582333a2c2a79
Lucas Felipe <lucas.miranda880@outlook.com>
2025-03-12 22:20:23 -0300

    added the function to convert from miles to kilometers

--- Week 2/odomenter_calculator.py
+++ Week 2/odomenter_calculator.py
@@ -4,11 +4,15 @@ def main():
     gallons = float(input("Enter the number of gallons of gas used: "))
     mpg2 = miles_per_gallon(start, end, gallons)
     print(f"The miles per gallon is {mpg2:.2f}")
+    print(f"{lp100k_from_mpg(mpg2):.2f} liters per 100 kilometers")
 
 
 def miles_per_gallon(start_miles, end_miles, gallons):
     mpg = (end_miles - start_miles) / gallons
     
     return mpg
+def lp100k_from_mpg(mpg):
+    lp100k = 235.215 / mpg
+    return lp100k
 
 main()
