03cf46c4129b8d35e045da58656a31a8c3d40b8b
Lucas Felipe <lucas.miranda880@outlook.com>
2025-03-14 10:50:10 -0300

    I was able to create a for loop that will run the comand 12 times and print the efficiency but this is not what I want. I'll likely need to store the information within a list and then print the list

--- can_size.py
+++ can_size.py
@@ -1,4 +1,14 @@
 import math
+def main():
+     for i in range(13):
+        radius = float(input("Please enter the radius of the can: "))
+        height = float(input("Please enter the height of the can: "))
+        volume = can_volume(radius, height)
+        ex_surface_area = can_surface_area(radius, height)
+        efficiency = storage_efficiency(volume, ex_surface_area)
+        print(f"Volume: {volume: .2f}")
+        print(f"Exterior Surface Area: {ex_surface_area: .2f}")
+        print(f"Storage Efficiency: {efficiency:.2f}")
 def can_volume(radius, height):
     volume = math.pi * radius**2 * height
     return volume
@@ -11,14 +21,7 @@ def storage_efficiency(volume, surface_area):
     efficiency = volume / surface_area
     return efficiency
 
-def main():
-    radius = float(input("Please enter the radius of the can: "))
-    height = float(input("Please enter the height of the can: "))
-    volume = can_volume(radius, height)
-    ex_surface_area = can_surface_area(radius, height)
-    efficiency = storage_efficiency(volume, ex_surface_area)
-    print(f"Volume: {volume}")
-    print(f"Exterior Surface Area: {ex_surface_area}")
-    print(f"Storage Efficiency: {efficiency}")
+
+
 
 main()
