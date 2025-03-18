427398c1d3c4c0de2e6778cc2fc104138fb4b9d7
Lucas Felipe <lucas.miranda880@outlook.com>
2025-03-17 23:37:44 -0300

    added the main function to my weather.py program, as this is best practices

--- Week 3/weather.py
+++ Week 3/weather.py
@@ -1,5 +1,8 @@
 def cels_from_fahr(farh):
     cels = (farh - 32) * 5/9
     return cels
-cels = cels_from_fahr(float(input("Enter the temperature in Fahrenheit: ")))
-print(f"The temperature in Celsius is {cels:.2f}.")
+def main():
+    cels = cels_from_fahr(float(input("Enter the temperature in Fahrenheit: ")))
+    print(f"The temperature in Celsius is {cels:.2f}.")
+
+main()
