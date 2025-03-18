bce9727d3ff7d40fbf73c32ec0df1a602168bc64
Lucas Felipe <lucas.miranda880@outlook.com>
2025-03-17 23:25:51 -0300

    created the test_weather.py file to test the funtion

--- Week 3/test_weather.py
+++ Week 3/test_weather.py
@@ -1,3 +1,14 @@
-def test_min():
-    assert min(1, 2) == 1
-    print("min PASSED")
+from weather import cels_from_fahr
+from pytest import approx
+import pytest
+
+def test_cels_from_fahr():
+    assert cels_from_fahr(32) == approx(0)
+    assert cels_from_fahr(25) == approx(-31.66667)
+    assert cels_from_fahr(0) == approx(-17.77778)
+    assert cels_from_fahr(70) == approx(21.11111)
+
+#Call the main function that is part of pytest so that the 
+#computer will execute the test function in this file
+
+pytest.main(["-v", "--tb=line", "-rN", "__file__"])
