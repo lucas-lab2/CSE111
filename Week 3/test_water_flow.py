632dc9477844113ea86ef7c08850c671f7ee28d6
Lucas Felipe <lucas.miranda880@outlook.com>
2025-03-21 21:09:58 -0300

    added both th pressure_gain_from_water_height function and test_pressure_gain_from_water_height function

--- Week 3/test_water_flow.py
+++ Week 3/test_water_flow.py
@@ -1,10 +1,15 @@
-from water_flow import water_column_height
+from water_flow import water_column_height, pressure_gain_from_water_height
 from pytest import approx 
 import pytest
 
 def test_water_column_height():
-    assert water_column_height(0, 0) == approx(0)
-    assert water_column_height(0, 10) == approx(7.5)
-    assert water_column_height(25, 0) == approx(25)
+    assert water_column_height(0.0, 0.0) == approx(0)
+    assert water_column_height(0.0, 10.0) == approx(7.5)
+    assert water_column_height(25.0, 0.0) == approx(25)
     assert water_column_height(48.3, 12.8) == approx(57.9)
 
+def test_pressure_gain_from_water_height():
+    """Test the pressure_gain_from_water_height function with various heights."""
+    assert pressure_gain_from_water_height(0.0) == approx(0.000, abs=0.001)
+    assert pressure_gain_from_water_height(30.2) == approx(295.628, abs=0.001)
+    assert pressure_gain_from_water_height(50.0) == approx(489.450, abs=0.001)
--- Week 3/water_flow.py
+++ Week 3/water_flow.py
@@ -1,3 +1,4 @@
+
 def water_column_height(tower_height, tank_height):
     """
     Calculate the height of a column of water from a tower height and a tank wall height.
@@ -11,4 +12,19 @@ def water_column_height(tower_height, tank_height):
     """
     h = tower_height + (3 * tank_height) / 4
     return h
+
+def pressure_gain_from_water_height(height):
+    """
+    Calculate the pressure caused by Earth's gravity pulling on the water stored in an elevated tank.
+    
+    Parameters:
+    height (float): The height of the water column in meters.
+    
+    Returns:
+    float: The pressure in kilopascals.
+    """
+    P = (998.2 * 9.80665 * height) / 1000
+    return P
+
+
 
