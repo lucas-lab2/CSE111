c442108f71a0d4f802f08d87b9f7cff10c615f78
Lucas Felipe <lucas.miranda880@outlook.com>
2025-03-21 21:47:23 -0300

    created both the ressure_loss_from_pipe and the test_pressure_loss_from_pipe functions

--- Week 3/test_water_flow.py
+++ Week 3/test_water_flow.py
@@ -1,4 +1,4 @@
-from water_flow import water_column_height, pressure_gain_from_water_height
+from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe
 from pytest import approx 
 import pytest
 
@@ -13,3 +13,14 @@ def test_pressure_gain_from_water_height():
     assert pressure_gain_from_water_height(0.0) == approx(0.000, abs=0.001)
     assert pressure_gain_from_water_height(30.2) == approx(295.628, abs=0.001)
     assert pressure_gain_from_water_height(50.0) == approx(489.450, abs=0.001)
+
+def test_pressure_loss_from_pipe():
+    """Test the pressure_loss_from_pipe function with various inputs."""
+    assert pressure_loss_from_pipe(0.048692, 0.00, 0.018, 1.75) == approx(0.000, abs=0.001)
+    assert pressure_loss_from_pipe(0.048692, 200.00, 0.000, 1.75) == approx(0.000, abs=0.001)
+    assert pressure_loss_from_pipe(0.048692, 200.00, 0.018, 0.00) == approx(0.000, abs=0.001)
+    assert pressure_loss_from_pipe(0.048692, 200.0, 0.018, 1.75) == approx(-113.008, abs=0.001)
+    assert pressure_loss_from_pipe(0.048692, 200.00, 0.018, 1.65) == approx(-100.462, abs=0.001)
+    assert pressure_loss_from_pipe(0.286870, 1000.00, 0.013, 1.65) == approx(-61.576, abs=0.001)
+    assert pressure_loss_from_pipe(0.286870, 1800.75, 0.013, 1.65) == approx(-110.884, abs=0.001)
+    
--- Week 3/water_flow.py
+++ Week 3/water_flow.py
@@ -26,5 +26,22 @@ def pressure_gain_from_water_height(height):
     P = (998.2 * 9.80665 * height) / 1000
     return P
 
+def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_velocity):
+    """
+    Calculate the water pressure lost due to friction between the water and the walls of a pipe.
+    
+    Parameters:
+    pipe_diameter (float): The diameter of the pipe in meters.
+    pipe_length (float): The length of the pipe in meters.
+    friction_factor (float): The pipe’s friction factor.
+    fluid_velocity (float): The velocity of the water flowing through the pipe in meters/second.
+    
+    Returns:
+    float: The lost pressure in kilopascals.
+    """
+    P = (-friction_factor * pipe_length * 998.2 * fluid_velocity**2) / (2000 * pipe_diameter)
+    return P
+
+
 
 
