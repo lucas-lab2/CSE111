8fe8231de7f96840276fa7e95dd2363ebf86ad57
Lucas Felipe <lucas.miranda880@outlook.com>
2025-03-24 10:36:29 -0300

    testing loops and lists

--- Week 4/list_test.py
+++ Week 4/list_test.py
@@ -10,7 +10,19 @@
 # print(names)
 # print(presenters)
 
-person = {'first': 'Christopher'}
-person ['last'] = 'Harrison'
-print(person)
-print(person['first'])
+# person = {'first': 'Christopher'}
+# person ['last'] = 'Harrison'
+# print(person)
+# print(person['first'])
+
+# for name in ['Christopher', 'Susan']:
+#     print(name)
+
+# for index in range(0, 2):
+#     print(index)
+
+names = ['Christopher', 'Susan']
+index = 0
+while index < len(names):
+    print(names[index]) # print the name at the current index
+    index = index + 1
