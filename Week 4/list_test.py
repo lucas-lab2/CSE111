167ec22b129f7549fdae81c95be8593ba35e50ca
Lucas Felipe <lucas.miranda880@outlook.com>
2025-03-24 10:20:45 -0300

    testing some list functions

---
+++ Week 4/list_test.py
@@ -0,0 +1,11 @@
+# names = ['Susan', 'Christopher']
+# print(len(names))
+# names.insert(0, 'Bill')
+# print(names)
+# names.sort()
+# print(names) 
+
+names =['Susan', 'Christopher', 'Bill', 'Justin']
+presenters = names[1:3]
+print(names)
+print(presenters)
