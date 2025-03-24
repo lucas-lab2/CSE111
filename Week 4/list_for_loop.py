a5dcb4f6133516927a5fa7614f7990775a6ace92
Lucas Felipe <lucas.miranda880@outlook.com>
2025-03-24 10:52:10 -0300

    used the range function to go throught the elements of the list

--- Week 4/list_for_loop.py
+++ Week 4/list_for_loop.py
@@ -6,5 +6,29 @@ def main():
     for color in colors:
         print(color)
 # Call main to start this program.
+# if __name__ == "__main__":
+#     main()
+
+# The Python built-in range function creates and returns a sequence of numbers. 
+# The range function accepts one, two, or three parameters as shown in example 4 and its output. 
+# Many programmers use the range function in a for loop to cause the computer to repeat code once for each number in a range of numbers. 
+# Example 4 shows four for loops that iterate over a range of numbers.
+
+    # Count from zero to nine by one.
+    for i in range(10):
+        print(i)
+    print()
+    # Count from five to nine by one.
+    for i in range(5, 10):
+        print(i)
+    print()
+    # Count from zero to eight by two.
+    for i in range(0, 10, 2):
+        print(i)
+    print()
+    # Count from 100 down to 70 by three.
+    for i in range(100, 69, -3):
+        print(i)
+# Call main to start this program.
 if __name__ == "__main__":
     main()
