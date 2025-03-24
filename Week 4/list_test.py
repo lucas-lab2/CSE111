cc0cc097cedff47d159d763514496917c8597702
Lucas Felipe <lucas.miranda880@outlook.com>
2025-03-24 10:42:42 -0300

    Example contains a program that creates a list, prints the length of the list, retrieves and prints one item from the list, changes one item in the list, and then prints the entire list.

--- Week 4/list_test.py
+++ Week 4/list_test.py
@@ -27,9 +27,28 @@
 #     print(names[index]) # print the name at the current index
 #     index = index + 1
 
-names = ['Christopher', 'Susan']
-index = len(names)  # This line is unnecessary for the loop
+# names = ['Christopher', 'Susan']
+# index = len(names)  # This line is unnecessary for the loop
 
-for i in range(0, index):  # range(0, len(names)) will iterate over 0 and 1
-    print(names[i])
+# for i in range(0, index):  # range(0, len(names)) will iterate over 0 and 1
+#     print(names[i])
 
+# Example 1
+def main():
+    # Create a list that contains five strings.
+    colors = ["yellow", "red", "green", "yellow", "blue"]
+    # Call the built-in len function
+    # and print the length of the list.
+    length = len(colors)
+    print(f"Number of elements: {length}")
+    # Print the element that is stored
+    # at index 2 in the colors list.
+    print(colors[2])
+    # Change the element that is stored at
+    # index 3 from "yellow" to "purple".
+    colors[3] = "purple"
+    # Print the entire colors list.
+    print(colors)
+# Call main to start this program.
+if __name__ == "__main__":
+    main()
