47741aa969c49707be05f0fbaf713b184342f2e1
Lucas Felipe <lucas.miranda880@outlook.com>
2025-03-28 21:50:27 -0300

    I've modified again the chemestry file because some of the atomic mass values did not match with the required value for this activity.

--- Week 4/chemistry.py
+++ Week 4/chemistry.py
@@ -7,16 +7,16 @@ def make_periodic_table():
         ["As", "Arsenic", 74.9216],
         ["At", "Astatine", 210],
         ["Au", "Gold", 196.966569],
-        ["B", "Boron", 10.81],
+        ["B", "Boron", 10.811],  # Updated from 10.81
         ["Ba", "Barium", 137.327],
         ["Be", "Beryllium", 9.012182],
         ["Bi", "Bismuth", 208.9804],
         ["Br", "Bromine", 79.904],
-        ["C", "Carbon", 12.011],
+        ["C", "Carbon", 12.0107],  # Updated from 12.011
         ["Ca", "Calcium", 40.078],
         ["Cd", "Cadmium", 112.411],
         ["Ce", "Cerium", 140.116],
-        ["Cl", "Chlorine", 35.45],
+        ["Cl", "Chlorine", 35.453],  # Updated from 35.45
         ["Co", "Cobalt", 58.933195],
         ["Cr", "Chromium", 51.9961],
         ["Cs", "Cesium", 132.9054519],
@@ -24,44 +24,44 @@ def make_periodic_table():
         ["Dy", "Dysprosium", 162.5],
         ["Er", "Erbium", 167.259],
         ["Eu", "Europium", 151.964],
-        ["F", "Fluorine", 18.998403163],
+        ["F", "Fluorine", 18.9984032],  # Updated from 18.998403163
         ["Fe", "Iron", 55.845],
         ["Fr", "Francium", 223],
         ["Ga", "Gallium", 69.723],
         ["Gd", "Gadolinium", 157.25],
-        ["Ge", "Germanium", 72.63],
-        ["H", "Hydrogen", 1.008],
+        ["Ge", "Germanium", 72.64],  # Updated from 72.63
+        ["H", "Hydrogen", 1.00794],  # Updated from 1.008
         ["He", "Helium", 4.002602],
         ["Hf", "Hafnium", 178.49],
-        ["Hg", "Mercury", 200.592],
-        ["Ho", "Holmium", 164.93033],
+        ["Hg", "Mercury", 200.59],  # Updated from 200.592
+        ["Ho", "Holmium", 164.93032],  # Updated from 164.93033
         ["I", "Iodine", 126.90447],
         ["In", "Indium", 114.818],
         ["Ir", "Iridium", 192.217],
         ["K", "Potassium", 39.0983],
         ["Kr", "Krypton", 83.798],
         ["La", "Lanthanum", 138.90547],
-        ["Li", "Lithium", 6.94],
+        ["Li", "Lithium", 6.941],  # Updated from 6.94
         ["Lu", "Lutetium", 174.9668],
         ["Mg", "Magnesium", 24.305],
-        ["Mn", "Manganese", 54.938044],
-        ["Mo", "Molybdenum", 95.95],
-        ["N", "Nitrogen", 14.007],
+        ["Mn", "Manganese", 54.938045],  # Updated from 54.938044
+        ["Mo", "Molybdenum", 95.96],  # Updated from 95.95
+        ["N", "Nitrogen", 14.0067],  # Updated from 14.007
         ["Na", "Sodium", 22.98976928],
-        ["Nb", "Niobium", 92.90637],
+        ["Nb", "Niobium", 92.90638],  # Updated from 92.90637
         ["Nd", "Neodymium", 144.242],
         ["Ne", "Neon", 20.1797],
         ["Ni", "Nickel", 58.6934],
         ["Np", "Neptunium", 237],
-        ["O", "Oxygen", 15.999],
+        ["O", "Oxygen", 15.9994],  # Updated from 15.999
         ["Os", "Osmium", 190.23],
-        ["P", "Phosphorus", 30.973761998],
+        ["P", "Phosphorus", 30.973762],  # Updated from 30.973761998
         ["Pa", "Protactinium", 231.03588],
         ["Pb", "Lead", 207.2],
         ["Pd", "Palladium", 106.42],
         ["Pm", "Promethium", 145],
         ["Po", "Polonium", 209],
-        ["Pr", "Praseodymium", 140.90766],
+        ["Pr", "Praseodymium", 140.90765],  # Updated from 140.90766
         ["Pt", "Platinum", 195.084],
         ["Pu", "Plutonium", 244],
         ["Ra", "Radium", 226],
@@ -70,11 +70,11 @@ def make_periodic_table():
         ["Rh", "Rhodium", 102.9055],
         ["Rn", "Radon", 222],
         ["Ru", "Ruthenium", 101.07],
-        ["S", "Sulfur", 32.06],
+        ["S", "Sulfur", 32.065],  # Updated from 32.06
         ["Sb", "Antimony", 121.76],
-        ["Sc", "Scandium", 44.955908],
-        ["Se", "Selenium", 78.971],
-        ["Si", "Silicon", 28.085],
+        ["Sc", "Scandium", 44.955912],  # Updated from 44.955908
+        ["Se", "Selenium", 78.96],  # Updated from 78.971
+        ["Si", "Silicon", 28.0855],  # Updated from 28.085
         ["Sm", "Samarium", 150.36],
         ["Sn", "Tin", 118.71],
         ["Sr", "Strontium", 87.62],
@@ -82,16 +82,16 @@ def make_periodic_table():
         ["Tb", "Terbium", 158.92535],
         ["Tc", "Technetium", 98],
         ["Te", "Tellurium", 127.6],
-        ["Th", "Thorium", 232.0377],
+        ["Th", "Thorium", 232.03806],  # Updated from 232.0377
         ["Ti", "Titanium", 47.867],
-        ["Tl", "Thallium", 204.38],
-        ["Tm", "Thulium", 168.93422],
+        ["Tl", "Thallium", 204.3833],  # Updated from 204.38
+        ["Tm", "Thulium", 168.93421],  # Updated from 168.93422
         ["U", "Uranium", 238.02891],
         ["V", "Vanadium", 50.9415],
         ["W", "Tungsten", 183.84],
         ["Xe", "Xenon", 131.293],
-        ["Y", "Yttrium", 88.90584],
-        ["Yb", "Ytterbium", 173.045],
+        ["Y", "Yttrium", 88.90585],  # Updated from 88.90584
+        ["Yb", "Ytterbium", 173.054],  # Updated from 173.045
         ["Zn", "Zinc", 65.38],
         ["Zr", "Zirconium", 91.224],
     ]
